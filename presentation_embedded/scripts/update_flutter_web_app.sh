a3dcf59db5e56365043daa67bab38ca50660eb5c
lucas-goldner <lucas.goldner@googlemail.com>
2023-08-16 00:58:09 +0200

    feat: Finish update script with replacements

--- presentation_embedded/scripts/update_flutter_web_app.sh
+++ presentation_embedded/scripts/update_flutter_web_app.sh
@@ -3,6 +3,8 @@
 # Paths relative to the script's location
 SOURCE_DIR="../fluttershow_app/build/web/"
 DESTINATION_DIR="./public/flutter"
+MAIN_DART_FILE="$DESTINATION_DIR/main.dart.js"
+FLUTTER_FILE="$DESTINATION_DIR/flutter.js"
 
 # Check if the source directory exists
 if [ ! -d "$SOURCE_DIR" ]; then
@@ -26,4 +28,25 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Done."
+# Modify the flutter/main.dart.js file
+if [ -f "$MAIN_DART_FILE" ]; then
+    echo "Modifying $JS_FILE..."
+    sed -i '' 's|t1 = this._assetBase;|t1 = "/flutter/";|g' "$MAIN_DART_FILE"
+else
+    echo "Warning: $MAIN_DART_FILE does not exist. Skipping modification."
+fi
+
+if [ -f "$FLUTTER_FILE" ]; then
+    echo "Modifying $FLUTTER_FILE..."
+
+    # Remove the specific line
+    sed -i '' '/const base = document.querySelector("base");/d' "$FLUTTER_FILE"
+
+    # # Replace the specific line
+    sed -i '' 's|return (base && base.getAttribute("href")) \|\| "";|return "/flutter/";|g' "$FLUTTER_FILE"
+
+else
+    echo "Warning: $FLUTTER_FILE does not exist. Skipping modification."
+fi
+
+echo "Updated ✨"
