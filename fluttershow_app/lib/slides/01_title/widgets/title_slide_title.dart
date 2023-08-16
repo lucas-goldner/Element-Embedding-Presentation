3044b5d423c93f111641ab0675cd8a5324eae1c3
lucas-goldner <lucas.goldner@googlemail.com>
2023-08-17 01:44:02 +0200

    feat: Implement basic gltich shader effect on image

--- fluttershow_app/lib/slides/01_title/widgets/title_slide_title.dart
+++ fluttershow_app/lib/slides/01_title/widgets/title_slide_title.dart
@@ -1,56 +1,88 @@
+import 'dart:ui' as UI;
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_shaders/flutter_shaders.dart';
 import 'package:flutter_show/generated/l10n.dart';
 import 'package:flutter_show/styles/fs_text_styles.dart';
 
-class TitleSlideTitle extends StatelessWidget {
+class TitleSlideTitle extends HookWidget {
   const TitleSlideTitle({super.key});
+
+  Future<UI.Image> loadImage(String path) async {
+    final data = await rootBundle.load(path);
+    final bytes = data.buffer.asUint8List();
+    final codec = await UI.instantiateImageCodec(bytes);
+    final frameInfo = await codec.getNextFrame();
+    return frameInfo.image;
+  }
+
+  Future<(UI.Image, UI.Image)> loadImages() async {
+    final samplerOne = await loadImage('assets/images/custom/fluttershow.png');
+    final samplerTwo = await loadImage('assets/images/custom/distortion.png');
+    return (samplerOne, samplerTwo);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ShaderBuilder(
-      (context, shader, child) {
-        return AnimatedSampler(
-          (image, size, canvas) {
-            shader.setFloatUniforms((uniforms) {
-              uniforms
-                ..setFloat(200)
-                ..setFloat(200)
-                ..setSize(size);
-            });
+    final controller = useAnimationController(duration: 3.hours)..repeat();
 
-            shader.setImageSampler(0, image);
+    return FutureBuilder<(UI.Image, UI.Image)>(
+      future: loadImages(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          controller.forward();
 
-            canvas.drawRect(
-              Rect.fromLTWH(0, 0, size.width, size.height),
-              Paint()..shader = shader,
-            );
-          },
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Text(
-              S.of(context).presentationTitle,
-              style: FSTextStyles.title(),
-            )
-                .animate(
-                  autoPlay: true,
-                  onPlay: (controller) => controller.repeat(),
-                )
-                .shimmer(
-                  duration: 5.seconds,
-                  colors: [
-                    const Color.fromARGB(255, 255, 87, 221),
-                    const Color.fromARGB(255, 86, 194, 255),
-                    const Color.fromARGB(255, 255, 87, 221),
-                  ],
-                  stops: [0, 0.2, 1],
-                  curve: Curves.easeInQuad,
+          return ShaderBuilder(
+            (context, shader, child) {
+              controller.addListener(() {
+                shader.setFloat(3, controller.value);
+              });
+
+              return AnimatedSampler(
+                (image, size, canvas) {
+                  shader
+                    ..setFloatUniforms((uniforms) {
+                      uniforms.setSize(size);
+                    })
+                    ..setImageSampler(0, snapshot.requireData.$1)
+                    ..setImageSampler(1, snapshot.requireData.$2);
+
+                  canvas.drawRect(
+                    Rect.fromLTWH(0, 0, size.width, size.height),
+                    Paint()..shader = shader,
+                  );
+                },
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    S.of(context).presentationTitle,
+                    style: FSTextStyles.title(),
+                  )
+                      .animate(
+                        autoPlay: true,
+                        onPlay: (controller) => controller.repeat(),
+                      )
+                      .shimmer(
+                        duration: 5.seconds,
+                        colors: [
+                          const Color.fromARGB(255, 255, 87, 221),
+                          const Color.fromARGB(255, 86, 194, 255),
+                          const Color.fromARGB(255, 255, 87, 221),
+                        ],
+                        stops: [0, 0.2, 1],
+                        curve: Curves.easeInQuad,
+                      ),
                 ),
-          ),
-        );
+              );
+            },
+            assetKey: 'shaders/glitch.frag',
+          );
+        }
+
+        return const SizedBox.shrink();
       },
-      assetKey: 'shaders/glitch.frag',
     );
   }
 }
--- fluttershow_app/shaders/glitch.frag
+++ fluttershow_app/shaders/glitch.frag
@@ -1,20 +1,60 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 #version 460 core
 
-precision highp float;
+precision mediump float;
 
 #include <flutter/runtime_effect.glsl>
 
-layout(location = 0) uniform vec2 uPixels;
-layout(location = 1) uniform vec2 uSize;
-layout(location = 2) uniform sampler2D uTexture;
+uniform sampler2D iChannel0; 
+uniform sampler2D iChannel1; 
+uniform vec3 iResolution;
+uniform float iTime;
 
 out vec4 fragColor;
 
 void main() {
-  vec2 uv = FlutterFragCoord().xy / uSize;
-  vec2 puv = round(uv * uPixels) / uPixels;
-  fragColor = texture(uTexture, puv);
+    vec2 fragCoord = FlutterFragCoord();
+	vec2 uv = fragCoord.xy / iResolution.xy;
+	vec2 block = floor(fragCoord.xy / vec2(16));
+	vec2 uv_noise = block / vec2(64);
+	uv_noise += floor(vec2(iTime) * vec2(1234.0, 3543.0)) / vec2(64);
+	
+	float block_thresh = pow(fract(iTime * 1236.0453), 2.0) * 0.2;
+	float line_thresh = pow(fract(iTime * 2236.0453), 3.0) * 0.7;
+	
+	vec2 uv_r = uv, uv_g = uv, uv_b = uv;
+
+	// glitch some blocks and lines
+	if (texture(iChannel1, uv_noise).r < block_thresh ||
+		texture(iChannel1, vec2(uv_noise.y, 0.0)).g < line_thresh) {
+
+		vec2 dist = (fract(uv_noise) - 0.5) * 0.3;
+		uv_r += dist * 0.1;
+		uv_g += dist * 0.2;
+		uv_b += dist * 0.125;
+	}
+
+	fragColor.r = texture(iChannel0, uv_r).r;
+	fragColor.g = texture(iChannel0, uv_g).g;
+	fragColor.b = texture(iChannel0, uv_b).b;
+
+	// loose luma for some blocks
+	if (texture(iChannel1, uv_noise).g < block_thresh)
+		fragColor.rgb = fragColor.ggg;
+
+	// discolor block lines
+	if (texture(iChannel1, vec2(uv_noise.y, 0.0)).b * 3.5 < line_thresh)
+		fragColor.rgb = vec3(0.0, dot(fragColor.rgb, vec3(1.0)), 0.0);
+
+	// interleave lines in some blocks
+	if (texture(iChannel1, uv_noise).g * 1.5 < block_thresh ||
+		texture(iChannel1, vec2(uv_noise.y, 0.0)).g * 2.5 < line_thresh) {
+		float line = fract(fragCoord.y / 3.0);
+		vec3 mask = vec3(3.0, 0.0, 0.0);
+		if (line > 0.333)
+			mask = vec3(0.0, 3.0, 0.0);
+		if (line > 0.666)
+			mask = vec3(0.0, 0.0, 3.0);
+		
+		fragColor.xyz *= mask;
+	}
 }
