55cabc4e6710f3af235fc489cc6a27cb0575223f
lucas-goldner <lucas.goldner@googlemail.com>
2023-11-19 18:24:45 +0900

    feat: Update embedded presentation

--- presentation_embedded/public/flutter_init.js
+++ presentation_embedded/public/flutter_init.js
@@ -4,7 +4,9 @@ window.addEventListener("load", function (ev) {
   console.log("Initializing Flutter");
   _flutter.loader.loadEntrypoint({
     onEntrypointLoaded: async function (engineInitializer) {
-      let appRunner = await engineInitializer.initializeEngine();
+      let appRunner = await engineInitializer.initializeEngine({
+        useColorEmoji: true,
+      });
       await appRunner.runApp();
     },
   });
