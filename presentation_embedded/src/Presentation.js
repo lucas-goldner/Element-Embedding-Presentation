5ade3c3242828ed0b74815ca6bdda2cfc9ed1f05
lucas-goldner <lucas.goldner@googlemail.com>
2023-11-28 18:58:24 +0900

    feat: Add loading button for presentation

--- presentation_embedded/src/Presentation.js
+++ presentation_embedded/src/Presentation.js
@@ -3,10 +3,19 @@ import { Helmet } from "react-helmet-async";
 
 const Presentation = () => {
   return (
-    <Helmet>
-      <script src="/flutter/flutter.js" defer></script>
-      <script src="/flutter_init.js" defer></script>
-    </Helmet>
+    <>
+      <Helmet>
+        <script src="/flutter/flutter.js" defer></script>
+        <script src="/flutter_init.js" defer></script>
+      </Helmet>
+      <main>
+        <div className="button-container">
+          <button className="button" onClick={() => window.location.reload()}>
+            Load Presentation
+          </button>
+        </div>
+      </main>
+    </>
   );
 };
 
